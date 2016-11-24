b4429bbfc031e8a5b9833dcfc85ca6f7962a78bb
Jacob Killelea <jkillelea@protonmail.ch>
2016-11-23 19:28:06 -0800

    wifi_info header file for SSID and password

---
+++ lib/wifi_info/wifi_info.h
@@ -0,0 +1,7 @@
+#ifndef WIFI_INFO_h
+#define WIFI_INFO_h
+
+const char* HOME_SSID     = "..............";
+const char* HOME_PASSWORD = "..............";
+
+#endif /* end of include guard: WIFI_INFO_h */
